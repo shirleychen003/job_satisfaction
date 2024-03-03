62b04893320e9aeec81ad12efc25c85cff2ce579
Jessica <jessicajwooim@gmail.com>
2024-03-02 22:08:00 -0500

    improving and making the connected scatterplot

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -11,6 +11,8 @@
 #### Workspace setup ####
 library(tidyverse)
 library(janitor)
+library(ggplot2)
+library(dplyr)
 
 
 #### Simulate data ####
@@ -36,16 +38,16 @@ simulated_data
 simulated_data_clean <-
   tibble(
     "Year" = sample(
-      x = c("1989", "1998", "2006", "2016"),
+      x = c(1989, 1998, 2006, 2016),
       size = 4,
-      replace = TRUE
+      replace = FALSE
       ),
-    "Proportion Important (%)" = sample(
+    Important = sample(
       x = c(18:60),
       size = 4,
       replace = TRUE
     ),
-    "Proportion Very Important (%)" = sample(
+    Very_Important = sample(
       x = c(25:80),
       size = 4,
       replace = TRUE
@@ -55,3 +57,14 @@ simulated_data_clean <-
 simulated_data_clean
 
 
+###`# Graph Simulated Data ####
+simulated_data_clean |>
+  ggplot(mapping = aes(x = Year, y = Very_Important)) +
+  geom_line(color="grey") +
+  geom_point(shape=21, color="black", fill="#69b3a2", size=2) +
+  theme_minimal() +
+  labs(x = "Year", y = "Proportion Important (%)", caption = "Data source: World Bank.")
+
+
+
+`
